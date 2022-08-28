2798c85c02ed82f12e575c9d936dfc41a81c4fb2
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-08-27 20:44:14 -0400

    Add a timer + Change text in titleLabel.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,26 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    
     // Store the timings in a dictionnaries to make the code
     // more clean
-    let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    // Create a timer object
+    var timer:Timer = Timer()
+    
+    var secondsRemaining:Int = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle!
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        timer.invalidate() // Count only once per second
         
         switch hardness {
         case "Soft":
             print(eggTime[hardness]!)
+            secondsRemaining = eggTime[hardness]!
         case "Medium":
             print(eggTime[hardness]!)
+            secondsRemaining = eggTime[hardness]!
         case "Hard":
             print(eggTime[hardness]!)
+            secondsRemaining = eggTime[hardness]!
         default:
             print("Error")
         }
         
+        // Fire every seconds and repeat
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+    
+    // Used to count the passed seconds and invalidate the timer when
+    // it is due.
+    @objc func updateTimer() -> Void {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
+    
 }
 
 
